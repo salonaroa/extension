fc2307f11055def67d5e97c98074a63efe695ce9
Salona Roa <66857711+salonaroa@users.noreply.github.com>
2020-07-03 21:40:54 +0900

    Update contentScript.js

--- contentScript.js
+++ contentScript.js
@@ -1,15 +1,14 @@
 const url = new URL(window.location.href);
 const q = new URLSearchParams(url.search).get('q');
-document.body.innerHTML += `
-<iframe
-    id="salonaIframe"
-    src="https://salona.org/iframe?q=${q}"
-    style="border: none; width: 100%;"
-    scrolling="no"
-></iframe>`;
+const iframe = document.createElement('iframe')
+iframe.id = 'salonaIframe';
+iframe.src = `https://salona.org/iframe?q=${q}`;
+iframe.style = 'border: none; width: 100%;';
+iframe.scrolling = 'no';
+document.body.appendChild(iframe);
 window.onload = function() {
     const iframe = document.getElementById('salonaIframe');
-    iframe.contentWindow.postMessage('', 'https://salona.org');
+    iframe.contentWindow.postMessage(null, 'https://salona.org');
     window.addEventListener('message', function(event) {
         iframe.style.height = event.data;
     }, false);
